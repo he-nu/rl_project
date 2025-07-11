d79e910888efefd4227a6b2f7f14ef0138799099
he-nu <henri.nuortila@gmail.com>
2025-07-11 09:09:34 +0300

    Encapsulated hyperparameters

--- az_train.py
+++ az_train.py
@@ -5,46 +5,30 @@ from modules.alphazero import AlphaZeroParallel
 from modules.games import ConnectFour
 from modules.net import ResNet
 
+from config.AlphaZeroConfig import AlphaZeroConfig
 
-# Hyperparameters
-lr = 0.001
-weight_decay = 0.0001
-resnet_blocks = 12
-num_hidden = 256
-num_epochs = 20
-batch_size = 512
 
-num_searches = 800
-num_parallel_games = 100
-num_self_play_iterations = 600
-num_iterations = 15
 
-temperature = 0.5
-dirichlet_epsilon = 0.25
-dirichlet_alpha = 0.314159265358979
-C = 2
-# ----------------------------------------------------------------------
-
-def train():
+def train(config: AlphaZeroConfig):
     game = ConnectFour()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device {device}")
 
-    model = ResNet(game, resnet_blocks, num_hidden, device=device)
-    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    model = ResNet(game, config.resnet_blocks, config.num_hidden, device=device)
+    optimizer = Adam(model.parameters(), lr=config.lr, weight_decay=config.weight_decay)
 
 
     args = {
-        'C': C,
-        'num_searches': num_searches,
-        'num_iterations': num_iterations,
-        'num_self_play_iterations': num_self_play_iterations,
-        "num_parallel_games": num_parallel_games,
-        'num_epochs': num_epochs,
-        'batch_size': batch_size,
-        'temperature': temperature,
-        'dirichlet_epsilon': dirichlet_epsilon,
-        'dirichlet_alpha': dirichlet_alpha
+        'C': config.C,
+        'num_searches': config.num_searches,
+        'num_iterations': config.num_iterations,
+        'num_self_play_iterations': config.num_self_play_iterations,
+        "num_parallel_games": config.num_parallel_games,
+        'num_epochs': config.num_epochs,
+        'batch_size': config.batch_size,
+        'temperature': config.temperature,
+        'dirichlet_epsilon': config.dirichlet_epsilon,
+        'dirichlet_alpha': config.dirichlet_alpha
     }
 
     alpha_zero = AlphaZeroParallel(model, optimizer, game, args)
---
+++ config/AlphaZeroConfig.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AlphaZeroConfig:
+    # Training hyperparameters
+    lr: float = 0
+    weight_decay: float = 0.0001
+    resnet_blocks: int = 12
+    num_hidden: int = 256
+    num_epochs: int = 20
+    batch_size: int = 512
+
+    # Model parameters
+    num_searches: int = 800
+    num_parallel_games: int = 100
+    num_self_play_iterations: int = 600
+    num_iterations: int = 15
+
+    # MCTS parameters
+    temperature = 0.5
+    dirichlet_epsilon = 0.25
+    dirichlet_alpha = 0.314159265358979
+    C = 2
+
+    
+
